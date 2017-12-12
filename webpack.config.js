5845e45d0ec6723a7bc920b9df8f41903b2147c9
kirin <ywl_allen@163.com>
2017-12-12 17:39:24 +0800

    压缩插件与命令行压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 var HtmlPlugin = require('html-webpack-plugin'), // 自动生成html
-    ExtractTextPlugin = require('extract-text-webpack-plugin'); // 分离css
+    ExtractTextPlugin = require('extract-text-webpack-plugin'), // 分离css
+    BabiliPlugin = require('babili-webpack-plugin'); // 压缩打包后的js或使用命令行webpack -p进行压缩
 
 module.exports = {
     entry: __dirname + '/src/main',
     output: {
         filename: 'bundle.js',
         path: __dirname + '/dist'
+    },
+    // 检查文件大小与提示
+    performance: {
+        hints: 'error',
+        maxEntrypointSize: 1024 * 200, 
+        maxAssetSize: 1024 * 500
     },
     module: {
         rules: [{
@@ -23,6 +30,7 @@ module.exports = {
     },
     plugins: [
         new HtmlPlugin(),
-        new ExtractTextPlugin('style.css')
+        new ExtractTextPlugin('style.css'),
+        new BabiliPlugin()
     ]
 };
