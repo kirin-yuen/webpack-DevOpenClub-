100b57a594c0f9653257e4c0bc6b83e97b4b3ccf
kirin <ywl_allen@163.com>
2017-12-13 09:58:32 +0800

    分离外部引用依赖文件与应用程序

---
+++ dist/vendor.js
@@ -0,0 +1,1 @@
+webpackJsonp([1],{5:function(a,b,c){a.exports=c(0)}},[5]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 var HtmlPlugin = require('html-webpack-plugin'), // 自动生成html
     ExtractTextPlugin = require('extract-text-webpack-plugin'), // 分离css
-    BabiliPlugin = require('babili-webpack-plugin'); // 压缩打包后的js或使用命令行webpack -p进行压缩
+    BabiliPlugin = require('babili-webpack-plugin'), // 压缩打包后的js或使用命令行webpack -p进行压缩
+    Webpack = require('webpack');
 
 module.exports = {
-    entry: __dirname + '/src/main',
+    entry: {
+        app: __dirname + '/src/main',
+        vendor: ['jquery']
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js', // 若hardcode为bundle会报"Conflict: Multiple assets emit to the same filename bundle.js"
         path: __dirname + '/dist'
     },
     // 检查文件大小与提示
@@ -31,6 +35,9 @@ module.exports = {
     plugins: [
         new HtmlPlugin(),
         new ExtractTextPlugin('style.css'),
-        new BabiliPlugin()
+        new BabiliPlugin(),
+        new Webpack.optimize.CommonsChunkPlugin({
+            name: ['common'] // 文件名，和合并到vendor
+        }) // 公共部分代码优化合并
     ]
 };
